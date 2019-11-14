746f64129ccb12875f067ecf6055bd92ab55a77d
tmxiaopai <tmxiaopai@gmail.com>
2019-11-14 22:44:12 +0800

    Initialization

---
+++ src/test/java/com/oj/OjSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
