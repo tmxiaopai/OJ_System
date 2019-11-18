5bbd6a0b3c312b317f9d61c65e968b081ff591e1
tmxiaopai <tmxiaopai@gmail.com>
2019-11-18 08:35:51 +0800

    页面配置

--- src/main/java/com/oj/controller/UserController.java
+++ src/main/java/com/oj/controller/UserController.java
@@ -2,14 +2,35 @@ package com.oj.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
     @RequestMapping("/")
-    @ResponseBody
-    public ModelAndView index(){
-        return new ModelAndView("test");
+    public String home(){
+        return "ordinaryMenu/home";
+    }
+    @RequestMapping("/home_menu")
+    public String homeMenu(){
+        return "ordinaryMenu/home_menu";
+    }
+    @RequestMapping("/contest_list")
+    public String contestMenu(){
+        return "ordinaryContest/contest_list";
+    }
+    @RequestMapping("/subject_menu")
+    public String subjectMenu(){
+        return "ordinaryMenu/subject_menu";
+    }
+    @RequestMapping("/notice")
+    public String notice(){
+        return "ordinaryHome/notice";
+    }
+    @RequestMapping("/faq")
+    public String faq(){
+        return "ordinaryHome/faq";
+    }
+    @RequestMapping("/sub")
+    public String sub(){
+        return "ordinarySubject/submitandjudge";
     }
 }
--- src/test/java/com/oj/OjSystemApplicationTests.java
+++ src/test/java/com/oj/OjSystemApplicationTests.java
@@ -2,12 +2,17 @@ package com.oj;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootTest
 class OjSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@RequestMapping("/faq")
+	public String faq(){
+		return "ordinaryHome/faq";
 	}
 
 }
