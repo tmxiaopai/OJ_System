7912ade46b89390ab4f90e446d164ecc589556cb
tmxiaopai <tmxiaopai@gmail.com>
2019-11-19 16:25:32 +0800

    update data

--- src/main/java/com/oj/controller/Init.java
+++ src/main/java/com/oj/controller/Init.java
@@ -12,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Controller
@@ -30,6 +28,8 @@ public class Init {
     private ContestRepository contestRepository;
     @Autowired
     private ContestSubjectRepository contestSubjectRepository;
+    @Autowired
+    private NoticeRepository noticeRepository;
 
 
     @RequestMapping("init")
@@ -44,8 +44,8 @@ public class Init {
         OrdinaryUser ordinaryUser2 = new OrdinaryUser(0, 0, 0.0, user2);
         OrdinaryUser ordinaryUser3 = new OrdinaryUser(0, 0, 0.0, user3);
 
-        Administrator administrator1=new Administrator(user4);
-        Administrator administrator2=new Administrator(user5);
+        Administrator administrator1 = new Administrator(user4);
+        Administrator administrator2 = new Administrator(user5);
 
         userRepository.save(user1);
         userRepository.save(user2);
@@ -60,32 +60,31 @@ public class Init {
         administratorRepository.save(administrator1);
         administratorRepository.save(administrator2);
 
-        Subject subject1=new Subject(3679,"数字之积",0,0,0.0,1024,5000,"计算",(short) 0,"tmxf");
-        Subject subject2=new Subject(1293,"生日礼物",0,0,0.0,1024,5000,"计算",(short) 0,"tmxf");
-        Subject subject3=new Subject(1325,"采矿",0,0,0.0,1024,5000,"计算",(short) 0,"tmxf");
+        Subject subject1 = new Subject(3679, "数字之积", 0, 0, 0.0, 1024, 5000, "计算", (short) 0, "tmxf");
+        Subject subject2 = new Subject(1293, "生日礼物", 0, 0, 0.0, 1024, 5000, "计算", (short) 0, "tmxf");
+        Subject subject3 = new Subject(1325, "采矿", 0, 0, 0.0, 1024, 5000, "计算", (short) 0, "tmxf");
 
         subjectRepository.save(subject1);
         subjectRepository.save(subject2);
         subjectRepository.save(subject3);
 
-        Contest contest1 =new Contest("大学生程序设计选拔赛1","HGU University","这是一个比赛描述，字数可能有一二百字，字数不够只能复制来凑！",3,new Date(),new Date(),0,false,"123456",administrator1);
-        Contest contest2 =new Contest("大学生程序设计选拔赛2","HGU University","这是一个比赛描述，字数可能有一二百字，字数不够只能复制来凑！",3,new Date(),new Date(),0,false,"123456",administrator2);
-        Contest contest3 =new Contest("大学生程序设计选拔赛3","HGU University","这是一个比赛描述，字数可能有一二百字，字数不够只能复制来凑！",3,new Date(),new Date(),0,false,"123456",administrator1);
+        Contest contest1 = new Contest("大学生程序设计选拔赛1", "HGU University", "这是一个比赛描述，字数可能有一二百字，字数不够只能复制来凑！", 3, (Date) new Date(), (Date) new Date(), 0, false, "123456", administrator1);
+        Contest contest2 = new Contest("大学生程序设计选拔赛2", "HGU University", "这是一个比赛描述，字数可能有一二百字，字数不够只能复制来凑！", 3, (Date) new Date(), (Date) new Date(), 0, false, "123456", administrator2);
+        Contest contest3 = new Contest("大学生程序设计选拔赛3", "HGU University", "这是一个比赛描述，字数可能有一二百字，字数不够只能复制来凑！", 3, (Date) new Date(), (Date) new Date(), 0, false, "123456", administrator1);
 
         contestRepository.save(contest1);
         contestRepository.save(contest2);
         contestRepository.save(contest3);
 
-
-        ContestSubject contestSubject1=new ContestSubject(0,0,0.0,contest1,subject1);
-        ContestSubject contestSubject2=new ContestSubject(0,0,0.0,contest1,subject2);
-        ContestSubject contestSubject3=new ContestSubject(0,0,0.0,contest1,subject3);
-        ContestSubject contestSubject4=new ContestSubject(0,0,0.0,contest2,subject1);
-        ContestSubject contestSubject5=new ContestSubject(0,0,0.0,contest2,subject2);
-        ContestSubject contestSubject6=new ContestSubject(0,0,0.0,contest2,subject3);
-        ContestSubject contestSubject7=new ContestSubject(0,0,0.0,contest3,subject1);
-        ContestSubject contestSubject8=new ContestSubject(0,0,0.0,contest3,subject2);
-        ContestSubject contestSubject9=new ContestSubject(0,0,0.0,contest3,subject3);
+        ContestSubject contestSubject1 = new ContestSubject(0, 0, 0.0, contest1, subject1);
+        ContestSubject contestSubject2 = new ContestSubject(0, 0, 0.0, contest1, subject2);
+        ContestSubject contestSubject3 = new ContestSubject(0, 0, 0.0, contest1, subject3);
+        ContestSubject contestSubject4 = new ContestSubject(0, 0, 0.0, contest2, subject1);
+        ContestSubject contestSubject5 = new ContestSubject(0, 0, 0.0, contest2, subject2);
+        ContestSubject contestSubject6 = new ContestSubject(0, 0, 0.0, contest2, subject3);
+        ContestSubject contestSubject7 = new ContestSubject(0, 0, 0.0, contest3, subject1);
+        ContestSubject contestSubject8 = new ContestSubject(0, 0, 0.0, contest3, subject2);
+        ContestSubject contestSubject9 = new ContestSubject(0, 0, 0.0, contest3, subject3);
 
         contestSubjectRepository.save(contestSubject1);
         contestSubjectRepository.save(contestSubject2);
@@ -107,12 +106,25 @@ public class Init {
         contest3.getContestSubjects().add(contestSubject2);
         contest3.getContestSubjects().add(contestSubject3);
 
+        Notice notice1 = new Notice(new Date(), "Plantform Use", "zhetidaogonggaodeneirongshipinyin,yinweishurufayongbuliaolemeiyoubanfa.", administrator1);
+        Notice notice2 = new Notice(new Date(), "Plantform Use", "zhetidaogonggaodeneirongshipinyin,yinweishurufayongbuliaolemeiyoubanfa.", administrator1);
+        Notice notice3 = new Notice(new Date(), "Plantform Use", "zhetidaogonggaodeneirongshipinyin,yinweishurufayongbuliaolemeiyoubanfa.", administrator1);
 
+        noticeRepository.save(notice1);
+        noticeRepository.save(notice3);
+        noticeRepository.save(notice2);
 
+        administrator1.getNotices().add(notice1);
+        administrator1.getNotices().add(notice2);
+        administrator1.getNotices().add(notice3);
 
+        contestRepository.save(contest1);
+        contestRepository.save(contest2);
+        contestRepository.save(contest3);
 
+        administratorRepository.save(administrator1);
 
-
+        System.out.println(administrator1.getNotices().size());
 
         return "initsuccess";
 
